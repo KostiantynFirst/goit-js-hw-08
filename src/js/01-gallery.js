5107c7edc6bac05c62846c76293d039fa5a4e888
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2022-05-09 13:56:08 +0300

    Some changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems); const gallery = document.querySelector('div.gallery');
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+galleryItems.map(el => {
+    
+    // const galleryItem = document.createElement('div');
+    const galleryLink = document.createElement('a');
+    galleryLink.href = el.original;
+
+    const galleryImage = document.createElement('img');
+    
+    // galleryItem.classList.add('gallery__item');
+    galleryLink.classList.add('gallery__item');
+    
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = el.preview;
+    galleryImage.alt = el.description;
+    // galleryImage.title = el.description;
+
+    galleryLink.append(galleryImage);
+    // galleryItem.append(galleryLink);
+    
+    gallery.append(galleryLink);
+    return gallery;
+});
+
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt'});
+
